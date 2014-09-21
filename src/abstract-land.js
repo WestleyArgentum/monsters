c7a4ccdd4b5b537d07cdfef0258e39a9967dd95b
WestleyArgentum <WestleyArgentum@gmail.com>
2014-09-21 18:03:45 -0400

    fix quotes

--- src/abstract-land.js
+++ src/abstract-land.js
@@ -3,13 +3,13 @@
 
     var AbstractLand = root.AbstractLand = function(two) {
         var land = new Land(two);
-        Engine.addEntity(land, "abstractLand");
+        Engine.addEntity(land, 'abstractLand');
 
         var monster = new SquishyMonster(two, two.width / 4.0 * 3, two.height / 2.0);
-        Engine.addEntity(monster, "monster1");
+        Engine.addEntity(monster, 'monster1');
 
         var monster2 = new SquishyMonster(two, two.width / 4.0, two.height / 3.0);
-        Engine.addEntity(monster2, "monster2");
+        Engine.addEntity(monster2, 'monster2');
     }
 
     var Land = function(two) {
